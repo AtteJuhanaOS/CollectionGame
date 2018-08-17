a1bd64175fa15ca39f1ce568a769c0f11d07c9ea
Jutti100 <sussi2000@gmail.com>
2018-08-17 13:40:01 +0300

    Initial commit

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour {
+
+    public static int coinCount = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        ++CoinScript.coinCount;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.right * Time.deltaTime*70);
+    }
+
+    private void OnTriggerEnter(Collider colliderObject)
+    {
+        if(colliderObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+    void OnDestroy()
+    {
+        --CoinScript.coinCount;
+        if(CoinScript.coinCount<=0)
+        {
+            GameObject timer = GameObject.Find("LevelTimer");
+            Destroy(timer);
+
+            GameObject[] fireworkSystems = GameObject.FindGameObjectsWithTag("Firework");
+            foreach(GameObject currentFirework in fireworkSystems)
+            {
+                currentFirework.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimerScript : MonoBehaviour {
+
+    public float maxTime = 60.0f;
+    [SerializeField]
+    private float countDown;
+
+	// Use this for initialization
+	void Start () {
+        countDown = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countDown -= Time.deltaTime;
+
+        if(countDown<=0.0f)
+        {
+            CoinScript.coinCount = 0;
+            SceneManager.LoadScene("SampleScene");
+        }
+	}
+}
