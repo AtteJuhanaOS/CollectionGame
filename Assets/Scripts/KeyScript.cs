a386f98486c6b5dfd4e3c4c73c2c7a4392834f10
AtteTaajo <32447990+AtteTaajo@users.noreply.github.com>
2018-08-31 10:55:30 +0300

    key

---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour {
+
+    
+
+    void OnCollisionEnter(Collision PlayerObject)
+    {
+        if (PlayerObject.CompareTag("Player"))
+        {
+            PlayerObject.GetComponent<PlayerScript>().hasKey = true;
+
+                
+            
+
+            Destroy();
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
