6704fc5468234164c2515ce53dcfb54edb58b883
Jutti100 <sussi2000@gmail.com>
2018-08-31 09:54:58 +0300

    Added gate+sumting fun

---
+++ Assets/Scripts/GateScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateScript : MonoBehaviour
+{
+    public GameObject GateDoorObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    private void OnTriggerEnter(Collider ColliderObject)
+    {
+        if(ColliderObject.CompareTag("Player"))
+        {
+            if(ColliderObject.GetComponent<PlayerScript>().hasKey==true)
+            {
+                GateDoorObject.transform.localPosition = new Vector3(0, 7, 0.5f);
+            }
+        }
+    }
+}
