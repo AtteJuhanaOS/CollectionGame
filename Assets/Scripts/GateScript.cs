79c0e2fde21d49aac8fa6e9d65548255f2bacc28
Jutti100 <sussi2000@gmail.com>
2018-09-14 11:06:31 +0300

    Added sum shit

--- Assets/Scripts/GateScript.cs
+++ Assets/Scripts/GateScript.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class GateScript : MonoBehaviour
 {
-    public GameObject GateDoorObject;
+    public GameObject GateDoorObject1;
+    public GameObject GateDoorObject2;
+    private bool openDoor = false;
+    private float doorRotation1 = 90.0f;
+    private float doorRotation2 = 90.0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+		if(openDoor==true)
+            if(GateDoorObject1.transform.rotation.z <0.707)
+            {
+                Debug.Log(GateDoorObject1.transform.rotation.z);
+                doorRotation1 = doorRotation1 + (Time.deltaTime * 25);
+                doorRotation2 = doorRotation2 - (Time.deltaTime * 25);
+                GateDoorObject1.transform.localRotation = Quaternion.Euler(-90, 0, doorRotation1);
+                GateDoorObject2.transform.localRotation = Quaternion.Euler(-90, 0, doorRotation2);
+            }
 	}
     private void OnTriggerEnter(Collider ColliderObject)
     {
@@ -22,7 +34,7 @@ public class GateScript : MonoBehaviour
         {
             if(ColliderObject.GetComponent<PlayerScript>().hasKey==true)
             {
-                GateDoorObject.transform.localPosition = new Vector3(0, 7, 0.5f);
+                openDoor = true;
             }
         }
     }
